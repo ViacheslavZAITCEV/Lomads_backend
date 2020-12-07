019dac079d83cb16ae00f40257a7a7ac24df1fec
Cedric Alinc <cedric.alinc@gmail.com>
2020-12-07 12:05:07 +0100

    user & events models

---
+++ models/friends.js
@@ -0,0 +1,12 @@
+// var mongoose = require('./connection')
+
+// var friendSchema = mongoose.Schema({
+//     name: String,
+//     firstname: String,
+//     email: String,
+//     password: String,
+// })
+
+// var friendModel = mongoose.model('friends', friendSchema)
+
+// module.exports = friendModel;
---
+++ models/users.js
@@ -0,0 +1,24 @@
+var mongoose = require('./connection')
+
+var userSchema = mongoose.Schema({
+    nom: String,
+    prenom: String,
+    email: String,
+    mot_de_passe: String,
+    ville: String,
+    token : String,
+    salt: String,
+    avatar: String,
+    date_de_naissance : Date,
+    amis: [String],
+    preferences: [String],
+    confidentialite: Boolean,
+    favoris: [{ type: mongoose.Schema.Types.ObjectId, ref: 'events' }],
+    sorties : [{ type: mongoose.Schema.Types.ObjectId, ref: 'sorties' }],
+    groupes_amis:[{ type: mongoose.Schema.Types.ObjectId, ref: 'friends' }],
+    // conversations:[{ type: mongoose.Schema.Types.ObjectId, ref: 'conversations' }]
+})
+
+var userModel = mongoose.model('users', userSchema)
+
+module.exports = userModel;
