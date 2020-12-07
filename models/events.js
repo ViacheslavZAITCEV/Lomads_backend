404648db91dfcc2c85cdd28a8a44ea9ac04731e9
Mike Ostrowski <mickael.ostrowski@gmail.com>
2020-12-07 09:51:58 +0100

    first backend commit

---
+++ models/events.js
@@ -0,0 +1,12 @@
+// var mongoose = require('./connection')
+
+// var eventSchema = mongoose.Schema({
+//     name: String,
+//     firstname: String,
+//     email: String,
+//     password: String,
+// })
+
+// var eventModel = mongoose.model('events', eventSchema)
+
+// module.exports = eventModel;
