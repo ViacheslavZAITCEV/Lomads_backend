7e41f12bc2ae80b347e209e2bb53285d40694930
Cedric Alinc <cedric.alinc@gmail.com>
2020-12-08 11:28:12 +0100

    ajout des routes pour récupérer les evenements screen principal et carte détaillée

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+var userModel = require('../models/users')
+var eventModel = require('../models/events')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+// Route pour récupérer l'ensemble des évènements --> SCREEN des évènements
+// TESTE POSTMAN : OK
+router.get('/pullEvents', async function(req, res, next) {
+
+  const events = await eventModel.find()
+  
+  res.json(events);
+});
+
+
+// Route pour récupérer un évènement spécifiquement --> SCREEN de la carte évènement détaillée
+// TESTE POSTMAN : OK
+router.get('/pullEventDetaille', async function(req, res, next) {
+
+  const event = await eventModel.findById(req.query.id)
+ 
+  res.json(event);
+});
+
 
 module.exports = router;
